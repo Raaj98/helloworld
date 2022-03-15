3a0073fafeb0f02175958fb844fb69026661abb7
raaj98 <krraaj841998@gmail.com>
2022-03-15 13:28:52 +0530

    third commit

---
+++ Intro/Ifelsecondition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Ifelsecondition {
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Enter the value a");
+        int a = Scan.nextInt();
+        if (a<=100){
+            System.out.println("a is inside 100");
+        }
+        else {
+            System.out.println("a is not inside 100");
+        }
+    }
+}
+
