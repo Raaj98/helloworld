4576d67fad89c1da56bbb31e391e5959c1da5ca9
raaj98 <krraaj841998@gmail.com>
2022-03-15 11:30:35 +0530

    seventh commit

---
+++ Intro/Comparison.java
@@ -0,0 +1,23 @@
+class Comparison{
+    public static void main(String[]args){
+        int a=10,b=10;
+        if (a>b){
+            System.out.println("a is greater than b");
+        }
+        if (a<b){
+            System.out.println("a is less than b ");
+        }
+        if (a>=b){
+            System.out.println("a is greater than or equal to b");
+        }
+        if (a<=b){
+            System.out.println("a is less than or equal to b");
+        }
+        if (a==b){
+            System.out.println("a is equal to b");
+        }
+        if (a!=b){
+            System.out.println("a is not equal to b");
+        }
+    }
+}
--- Intro/TypeCasting.java
+++ Intro/TypeCasting.java
@@ -1,6 +1,7 @@
- class TypeCasting{
-    public static void main(String args[]){
-        int i=200;
-        String s=String.valueOf(i);
-        System.out.println(s);
-    }}
+ class TypeCasting {
+     public static void main(String args[]) {
+         int i = 200;
+         String s = String.valueOf(i);
+         System.out.println(s);
+     }
+ }
